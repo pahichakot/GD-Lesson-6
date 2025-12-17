12e28d37709b48b7535f38ae61b22616bc10346b
pahichakot <swarnalatha.palteru@gmail.com>
2025-12-16 21:09:12 -0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,91 @@
+#This code will ask user to input information for their group
+
+#Get input from user for group 1
+print("Group 1")
+sizeofthegroup1 = int(input("Please enter the number of people in the group : "))
+dateofthecompetition1 = input("Please enter the date of the competition (ex. 12-2) : ")
+venue1 = input("Please enter the venue of the group : ")
+typeofthemedal1 = input("Please enter the type of medal of the group : ")
+
+#Get input from user for group 2
+print("Group 2")
+sizeofthegroup2 = int(input("Please enter the number of people in the group : "))
+dateofthecompetition2 = input("Please enter the date of the competition (ex. 12-2) : ")
+venue2 = input("Please enter the venue of the group : ")
+typeofthemedal2 = input("Please enter the type of medal of the group : ")
+
+#Get input from user for group 3
+print("Group 3")
+sizeofthegroup3 = int(input("Please enter the number of people in the group : "))
+dateofthecompetition3 = input("Please enter the date of the competition (ex. 12-2) : ")
+venue3 = input("Please enter the venue of the group : ")
+typeofthemedal3 = input("Please enter the type of medal of the group : ")
+
+#Get input from user for group 4
+print("Group 4")
+sizeofthegroup4 = int(input("Please enter the number of people in the group : "))
+dateofthecompetition4 = input("Please enter the date of the competition (ex. 12-2) : ")
+venue4 = input("Please enter the venue of the group : ")
+typeofthemedal4 = input("Please enter the type of medal of the group : ")
+
+#Get input from user for group 5
+print("Group 5")
+sizeofthegroup5 = int(input("Please enter the number of people in the group : "))
+dateofthecompetition5 = input("Please enter the date of the competition (ex. 12-2) : ")
+venue5 = input("Please enter the venue of the group : ")
+typeofthemedal5 = input("Please enter the type of medal of the group : ")
+
+#Create tuple for Group 1
+group1 = (sizeofthegroup1, dateofthecompetition1, venue1, typeofthemedal1)
+
+sizeofthegroup1, dateofthecompetition1, venue1, typeofthemedal1 = group1
+print("Group 1 :\n")
+print("Size of the group : ", sizeofthegroup1)
+print("Date of the Competition : ", dateofthecompetition1)
+print("Venue : ", venue1)
+print("Type of medal : ", typeofthemedal1)
+print("\n")
+
+#Create tuple for Group 2
+group2 = (sizeofthegroup2, dateofthecompetition2, venue2, typeofthemedal2)
+
+sizeofthegroup2, dateofthecompetition2, venue2, typeofthemedal2 = group2
+print("Group 2 :\n")
+print("Size of the group : ", sizeofthegroup2)
+print("Date of the Competition : ", dateofthecompetition2)
+print("Venue : ", venue2)
+print("Type of medal : ", typeofthemedal2)
+print("\n")
+
+#Create tuple for Group 3
+group3 = (sizeofthegroup3, dateofthecompetition3, venue3, typeofthemedal3)
+
+sizeofthegroup3, dateofthecompetition3, venue3, typeofthemedal3 = group3
+print("Group 3 :\n")
+print("Size of the group : ", sizeofthegroup3)
+print("Date of the Competition : ", dateofthecompetition3)
+print("Venue : ", venue3)
+print("Type of medal : ", typeofthemedal3)
+print("\n")
+
+#Create tuple for Group 4
+group4 = (sizeofthegroup4, dateofthecompetition4, venue4, typeofthemedal4)
+
+sizeofthegroup4, dateofthecompetition4, venue4, typeofthemedal4 = group4
+print("Group 4 :\n")
+print("Size of the group : ", sizeofthegroup4)
+print("Date of the Competition : ", dateofthecompetition4)
+print("Venue : ", venue4)
+print("Type of medal : ", typeofthemedal4)
+print("\n")
+
+#Create tuple for Group 5
+group5 = (sizeofthegroup5, dateofthecompetition5, venue5, typeofthemedal5)
+
+sizeofthegroup5, dateofthecompetition5, venue5, typeofthemedal5 = group5
+print("Group 5 :\n")
+print("Size of the group : ", sizeofthegroup5)
+print("Date of the Competition : ", dateofthecompetition5)
+print("Venue : ", venue5)
+print("Type of medal : ", typeofthemedal5)
+print("\n")
